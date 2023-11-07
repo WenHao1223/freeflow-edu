f3ae0cd26d3ab251602d9b3d99c1c9bfe5d84ba6
Lim Wen Hao <wenhaojshs@gmail.com>
2023-11-08 00:39:12 +0800

    fix ui bug

--- src/sharedComponents/HomeNavbar.jsx
+++ src/sharedComponents/HomeNavbar.jsx
@@ -7,13 +7,6 @@ import { Collapse, Dropdown, Tooltip, initTE } from "tw-elements";
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 
-// // solana
-// import { useTransferTokens } from '@lndgalante/solutils';
-// import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-// import { WalletMultiButton, WalletDisconnectButton } from '@solana/wallet-adapter-react-ui';
-
-// import Button from "../components/Button";
-
 const firebaseConfig = {
     apiKey: "AIzaSyAj-GUmYIXPUpAoFSAmQaiQ7to35EqqgvI",
     authDomain: "freeflow-edu.firebaseapp.com",
@@ -64,12 +57,12 @@ const HomeNavbar = (props) => {
     console.log(props.state.walletAddress);
     return (
         <>
-            <nav className="flex-no-wrap relative flex w-full items-center justify-between bg-[#FBFBFB] dark:bg-transparent lg:flex-wrap lg:justify-start mb-4">
+            <nav className="flex-no-wrap relative flex w-full items-center justify-between bg-transparent dark:bg-transparent lg:flex-wrap lg:justify-start mb-4">
                 <div className="flex w-full flex-wrap items-center justify-between px-3">
                     <button className="block border-0 bg-transparent px-2 text-neutral-500 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden" type="button" data-te-collapse-init data-te-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="[&>svg]:w-7">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-7 w-7">
-                            <path fill-rule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+                            <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
                             </svg>
                         </span>
                     </button>
