8f7026ec40d1173ebcae2353865f76903a18dc24
Lim Wen Hao <wenhaojshs@gmail.com>
2023-11-08 00:17:15 +0800

    update upload.jsx ui

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,11 +1,12 @@
 import React, { Component, useEffect, useState } from "react";
 import withRouter from "../sharedComponents/withRouter";
-// import { useNavigate, Navigate } from "react-router-dom";
 
 // firebase
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, serverTimestamp } from 'firebase/firestore';
 import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+import { Ripple, initTE } from "tw-elements";
 
 // solana
 import SOLtoUSD from "./SOLtoUSD";
@@ -39,11 +40,13 @@ class Upload extends Component {
             notesUpload: null,
             sol: 1
         }
+
+        initTE({ Ripple });
     }
 
     select = (opt) => {
         $(".button_choice").attr("class", "button_choice");
-        $(`#b_${opt}`).attr("class", "button_choice text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800");
+        $(`#b_${opt}`).attr("class", "button_choice text-white bg-secondary-700 hover:bg-secondary-800 focus:ring-4 focus:ring-secondary-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-secondary-600 dark:hover:bg-secondary-700 focus:outline-none dark:focus:ring-secondary-800");
         
         this.setState({
             choice: opt
@@ -70,7 +73,8 @@ class Upload extends Component {
             this.setState({
                 tag: this.state.tag.add($("#t_tag").val())
             });
-            $("#t_tag").val("")
+            $("#t_tag").val("");
+            $("#div_tag").css("display", "inline-block");
         }
     }
 
@@ -165,92 +169,104 @@ class Upload extends Component {
         console.log(this.state);
         const tagCards = (this.state.tag.size === 0) ? [] : [...this.state.tag].map((item, pos) => {
             return (
-                <span key={item} id={"tag_"+item}>
+                <span data-te-ripple-init data-te-ripple-color="light" className="inline-block m-2 ms-3 me-1 text-sm rounded bg-neutral-50 px-6 pb-2 pt-2.5 leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-neutral-100 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]" key={item} id={"tag_"+item}>
                     {item}
-                    <button onClick={() => this.removeTag(item)}>x</button>
+                    <button data-te-ripple-init data-te-ripple-color="light" className="ms-2 inline-block rounded px-3 pb-1 pt-1.5 font-medium leading-normal text-danger transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.3),0_4px_18px_0_rgba(220,76,100,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(220,76,100,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(220,76,100,0.2),0_4px_18px_0_rgba(220,76,100,0.1)]" onClick={() => this.removeTag(item)}>x</button>
                 </span>
             );
         });
 
         return(
-            <section id="main">
-                <h1>Upload</h1>
+            <section id="main" className="max-w-xl mx-auto divide-y md:max-w-4xl">
+                <div className="py-8 border-b">
+                    <h1 className="text-4xl font-bold">Upload</h1>
+                    <p className="mt-3 text-lg text-gray-500">Sculpting Futures, Unleasing Knowledge: FreeFlow Edu, Where Learning Knows No Bounds!</p>
+                </div>
 
-                <div className="div_general">
-                    <label> Course Title: 
-                        <input onKeyUp={this.handleKeyPress} type="text" name="t_tile" id="t_title" placeholder="Trigonometry" required/>
-                    </label>
-                    <br/>
-                    <label> Course Description: 
-                        <textarea name="t_des" id="t_des" placeholder="Write something here..." required/>
-                    </label>
-                    <br/>
-                    <label> Level: 
-                        <select name="" id="c_level" required>
-                            <option value="">-- Please select --</option>
-                            <option value="primary">Primary School</option>
-                            <option value="secondary">Secondary School</option>
-                            <option value="undergraduate">Undergraduate</option>
-                            <option value="postgraduate">Postgraduate</option>
-                            <option value="workplace">Workplace</option>
-                        </select>
-                    </label>
-                    <label> Subject: 
-                        <input onKeyUp={this.handleKeyPress} type="text" name="t_sub" id="t_sub" placeholder="Mathematics" required/>
-                    </label>
-                    <label> Language: 
-                        <input onKeyUp={this.handleKeyPress} type="text" name="t_lang" id="t_lang" placeholder="English" required/>
-                    </label>
-                    <br />
-                    <label> Sell for $: 
-                        <input onKeyUp={this.handleKeyPress} onChange={this.changeSOL} type="number" name="t_sol" id="t_sol" placeholder="0.00" required/> Solana
-                        <br />{this.state.sol} SOL = {<SOLtoUSD sol={this.state.sol}/>} USD
-                    </label>
-                    <br/>
-                    <label> Tag: 
-                        <input onKeyUp={this.handleTagKeyPress} type="text" name="t_tag" id="t_tag" placeholder="Write some keywords here..."/>
-                    </label>
-                    <div id="div_tag">
-                        {tagCards}
+                <div className="div_general py-6">
+                    <h2 className="text-2xl font-bold">General</h2>
+                    <p className="m5-3 text-lg text-gray-500">This is where the course description will be displayed to the Homepage of our FreeFlow Edu App.</p>
+                    <div className="mt-8 max-w-md">
+                        <div className="grid grid-cols-1 gap-6">
+                            <label className="block">
+                                <span className="text-gray-600">Course Title: </span>
+                                <input onKeyUp={this.handleKeyPress} type="text" name="t_tile" id="t_title" className="mt-1 block w-full" placeholder="Trigonometry" required/>
+                            </label>
+                            <label className="block">
+                                <span className="text-gray-600">Course Description: </span>
+                                <textarea name="t_des" id="t_des" className="mt-1 block w-full" placeholder="Write something here..." required/>
+                            </label>
+                            <label className="block">
+                                <span className="text-gray-600">Level: </span>
+                                <select className="block w-full mt-1" id="c_level" required>
+                                    <option value="">-- Please select --</option>
+                                    <option value="primary">Primary School</option>
+                                    <option value="secondary">Secondary School</option>
+                                    <option value="undergraduate">Undergraduate</option>
+                                    <option value="postgraduate">Postgraduate</option>
+                                    <option value="workplace">Workplace</option>
+                                </select>
+                            </label>
+                            <label className="block">
+                                <span className="text-gray-600">Subject: </span>
+                                <input onKeyUp={this.handleKeyPress} type="text" name="t_sub" id="t_sub" className="mt-1 block w-full" placeholder="Mathematics" required/>
+                            </label>
+                            <label className="block">
+                                <span className="text-gray-600">Language: </span>
+                                <input onKeyUp={this.handleKeyPress} type="text" name="t_lang" id="t_lang" className="mt-1 block w-full" placeholder="English" required/>
+                            </label>
+                            <label className="block">
+                                <span className="text-gray-600">Sell for Solana of : </span>
+                                <input onKeyUp={this.handleKeyPress} onChange={this.changeSOL} type="number" name="t_sol" id="t_sol" className="mt-1 block w-full" placeholder="0.00" required/>
+                                <br />{this.state.sol} SOL = {<SOLtoUSD sol={this.state.sol}/>} USD
+                            </label>
+                            <label className="block">
+                                <span className="text-gray-600">Tag: </span>
+                                <input onKeyUp={this.handleTagKeyPress} type="text" name="t_tag" id="t_tag" className="mt-1 block w-full" placeholder="Write some keywords here..."/>
+                            </label>
+                            <div style={{display: "none"}} id="div_tag">
+                                {tagCards}
+                            </div>
+                            <label className="block">
+                                <span className="text-gray-600">Author: </span>
+                                <input type="text" name="t_author_uid" id="t_author_uid" className="mt-1 block w-full" value={this.state.user.email} disabled/>
+                            </label>
+                            <label className="block" forhtml="f_notes">
+                                <span className="text-gray-600">Upload Thumbnail: </span>
+                                <input className="mt-1 block w-full relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary" type="file" id="f_thumbnail" accept=".jpg" onChange={(e) => this.setState({thumbnailUpload: e.target.files[0]})}/>
+                            </label>
+                        </div>
                     </div>
-                        <br/>
-                    <label> Author: 
-                        <input type="text" name="t_author_uid" id="t_author_uid" value={this.state.user.email} disabled/>
-                    </label>
-                    <br/>
-                    <label> Thumbnail: 
-                        <input type="file" onChange={(e) => this.setState({thumbnailUpload: e.target.files[0]})} name="vid" id="f_thumbnail" accept=".jpg"/>
-                    </label>
                 </div>
-                <br />
-                <hr />
-                <br />
 
-                <div>
-                    <button className="button_choice"  id="b_video" onClick={() => this.select("video")}>Video</button>
-                    <button className="button_choice"  id="b_notes" onClick={() => this.select("notes")}>Notes</button>
-                    <button className="button_choice"  id="b_both" onClick={() => this.select("both")}>Both</button>
+                <div className="py-6 grid grid-cols-3 gap-4">
+                    <button className="button_choice" id="b_video" onClick={() => this.select("video")}>Video</button>
+                    <button className="button_choice" id="b_notes" onClick={() => this.select("notes")}>Notes</button>
+                    <button className="button_choice" id="b_both" onClick={() => this.select("both")}>Both</button>
+                </div>
+
+                <div id="div_video" className="div_upload pb-2 !border-0" style={{display: "none"}}>
+                    <h2 className="text-2xl font-bold mb-4">Upload Video</h2>
+                    <div className="mb-3">
+                        <label forhtml="f_notes">
+                            <input className="mt-1 block w-full relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary" type="file" id="f_video" accept=".mp4" onChange={(e) => this.setState({videoUpload: e.target.files[0]})}/>
+                        </label>
+                    </div>
                 </div>
     
-                <div id="div_video" className="div_upload" style={{display: "none"}}>
-                    <h1>Video</h1>
-                    <label>Upload Video: 
-                        <input type="file" onChange={(e) => this.setState({videoUpload: e.target.files[0]})} name="vid" id="f_video" accept=".mp4"/>
-                    </label>
-                    <br />
+                <div id="div_notes" className="div_upload pb-2 !border-0" style={{display: "none"}}>
+                    <h2 className="text-2xl font-bold mb-4">Upload Notes</h2>
+                    <div className="mb-3">
+                        <label forhtml="f_notes">
+                            <input className="mt-1 block w-full relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary" type="file" id="f_notes" accept=".pdf" onChange={(e) => this.setState({notesUpload: e.target.files[0]})}/>
+                        </label>
+                    </div>
                 </div>
     
-                <div id="div_notes" className="div_upload" style={{display: "none"}}>
-                    <h1>Notes</h1>
-                    <label>Upload Notes: 
-                        <input type="file" onChange={(e) => this.setState({notesUpload: e.target.files[0]})} name="vid" id="f_notes" accept=".pdf"/>
-                    </label>
-                    <br />
+
+                <div className="py-6">
+                    <button className="inline-block rounded bg-primary px-8 pb-3 pt-4 font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]" onClick={this.uploadConfirm}>Upload</button>
                 </div>
-                <br />
-                <hr />
-                <br />
-                <button onClick={this.uploadConfirm}>Upload</button>
             </section>
         );
     }
