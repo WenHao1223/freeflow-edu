65beab064fa020e63413915ec5be7c6fa2423292
csheng <limcongsheng@gmail.com>
2023-11-08 23:19:16 +0800

    beautify profile

--- src/pages/SignOut.jsx
+++ src/pages/SignOut.jsx
@@ -36,8 +36,8 @@ class SignOut extends Component {
         if(this.state){
             return (
                 <div className="m-12 mt-4 flex items-center justify-center">
-                    <div className="p-4 sm:p-10 bg-gray-50 rounded-md  w-[300px] md:w-[500px] text-center overflow-y-auto">
-                        <span className="mb-4 inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-yellow-50 bg-yellow-100 text-yellow-500">
+                    <div className="p-4 sm:p-10 rounded-md h-[70vh] w-[80vw] md:w-[500px] text-center overflow-y-auto">
+                        <span className="mb-4 mt-[20vh] inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-danger-50 bg-danger-100 text-danger-500">
                             <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
                             </svg>
